e6859f44a2b939e928213c99a04407b78087999b
ElanurGuduk <49445642+GudukElanur@users.noreply.github.com>
2024-03-29 13:01:49 +0300

    #02 components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularProject';
+  title: string = 'AngularProject';
+  product1 = { productId: 1, productName: 'Laptop', categoryId: 1 };
+  product2 = { productId: 2, productName: 'Mouse', categoryId: 1 };
+
+  products = [this.product1, this.product2];
 }
